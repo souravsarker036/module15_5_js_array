ffa79815a7664df8173b6eafc5adf9c381c1822f
Sourav Sarker <souravsarker036@gmail.com>
2025-08-03 04:08:48 +0600

    push pop shif unshift

---
+++ push-pop.js
@@ -0,0 +1,14 @@
+// push
+// const age = [];
+// const numbers =[1, 2, 3, 4, 5];
+// console.log(numbers);
+// numbers.push(44);
+// numbers.push(45, 55, 65);
+// console.log(numbers)
+
+// pop
+const friends = ['balam', 'kalam', 'salam']
+console.log(friends)
+
+friends.pop();
+console.log(friends)
---
+++ shift.js
@@ -0,0 +1,8 @@
+const friends = ['balam', 'kalam', 'salam'];
+console.log(friends);
+
+friends.shift();     /*remove the first index*/
+console.log(friends);
+
+friends.unshift('gelam');     /*add value in the first index*/
+console.log(friends);
