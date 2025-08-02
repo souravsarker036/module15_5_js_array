5829d60b4cf76211bc17ac3e3694d81ed180f792
Sourav Sarker <souravsarker036@gmail.com>
2025-08-03 04:21:25 +0600

    includes indexof

---
+++ includes.js
@@ -0,0 +1,10 @@
+const friends = ['balam', 'kalam', 'salam'];
+
+console.log(friends.includes('kalam'))
+
+if(friends.includes('kalam')){
+    console.log('party is on')
+}
+else{
+    console.log("no party is here")
+}
---
+++ index-of.js
@@ -0,0 +1,4 @@
+const friends = ['balam', 'kalam', 'salam'];
+console.log(friends.indexOf('salam'))
+
+console.log(friends.indexOf('tomato'))  /* if there is no value its given the index -1 */
