6d109026874586f28c6640ca4af7bc2d89943b67
Sourav Sarker <souravsarker036@gmail.com>
2025-08-03 03:54:29 +0600

    array len index

---
+++ array.js
@@ -0,0 +1,2 @@
+const number = [12, 13, 22, 24, 25];
+console.log(number)
---
+++ length.js
@@ -0,0 +1,11 @@
+const numbers = [1, 4, 5, 6, 7, 8, 9, 10, 11];
+console.log(numbers.length);
+console.log(numbers[2]);
+
+//array er kono value k onno variable er moddhe rakha 
+// const fourth = numbers[3];
+// console.log(fourth)
+
+// change a valuer in array
+numbers[2] = 20;
+console.log(numbers)
